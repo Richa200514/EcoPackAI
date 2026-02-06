989fbebdd01c074e20a57f662d50d4abc10515a8
Tekulapally Richa <tekulapallyrichareddy@gmail.com>
2026-02-06 17:51:52 +0530

    Suppress ML feature-name warnings

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify, render_template
 import pandas as pd
 import matplotlib
+import warnings
 
+warnings.filterwarnings("ignore")
 matplotlib.use("Agg")
 
 import matplotlib.pyplot as plt
